7096f9f1cdd4b231b0b4cbd1093316c7d52ef6f6
janushan12 <janu.1999.12@gmail.com>
2024-11-20 12:08:22 +0530

    Initial commit with HTML, CSS, and JS files

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".text", {
+    strings: ["FullStack Developer", "Web Developer", "Mobile App Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+document.getElementById("currentYear").textContent = new Date().getFullYear();
