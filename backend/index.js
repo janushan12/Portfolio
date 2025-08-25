6f3ff45ac13b1a24dc3e952cbad7b020a35e867c
janushan12 <janu.1999.12@gmail.com>
2025-08-25 22:47:25 +0530

    Port value modified

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,7 @@ const app = express();
 app.use(cors({
     origin: "https://janushan12.github.io",
     methods: ["GET", "POST", "PUT", "OPTIONS"],
-    credentials: true
 }));
-
 app.options("*", cors())
 
 app.use(bodyParser.json());
@@ -45,7 +43,7 @@ app.post("/send", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=> {
     console.log(`Server running on port ${PORT}`);
