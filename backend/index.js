f32ac7f1e13c2b57ed1646752845e15a458cdfa0
janushan12 <janu.1999.12@gmail.com>
2025-08-25 17:58:27 +0530

    Added backend folder with initial code

---
+++ backend/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/send", async (req, res) => {
+    const { name, email, subject, message } = req.body;
+    try {
+        let transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
+            },
+        });
+
+        let mailOptions = {
+            From: email,
+            to: "janu.1999.12@gmail.com",
+            subject: `Portfolio Contact: ${name}`,
+            text: `You got a message from:
+            Name: ${name}
+            Email: ${email}
+            Subject: ${subject}
+            Message: ${message}`,
+        };
+        await transporter.sendMail(mailOptions);
+        res.status(200).json({ success: true, message: "Email sent successfully!" });
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Something went wrong!" });
+    }
+});
+
+app.listen(3000, ()=> {
+    console.log("Server running on port 3000");
+})
--- main.js
+++ main.js
@@ -7,3 +7,37 @@ var typed = new Typed(".text", {
 });
 
 document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+document.getElementById("contactForm").addEventListener("submit", async (e)=> {
+    e.preventDefault();
+
+    let submitBtn = document.querySelector(".send");
+
+    submitBtn.disabled = true;
+    submitBtn.textContent = "Submitting...";
+
+    const formData = {
+        name: e.target.name.value,
+        email: e.target.email.value,
+        subject: e.target.subject.value,
+        message: e.target.message.value
+    };
+
+    try {
+    const response = await fetch("http://localhost:3000/send", {
+        method: "POST",
+        headers: { "Content-Type": "Application/json" },
+        body: JSON.stringify(formData)
+    });
+
+    const result = await response.json();
+    document.getElementById("statusMessage").textContent = result.message;
+
+    e.target.reset();
+} catch (error) {
+    alert("Something went wrong. Please try again.");
+} finally {
+    submitBtn.disabled = false;
+    submitBtn.textContent = "Submit"
+}
+})
