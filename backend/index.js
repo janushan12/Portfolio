e457292d915cba41ff556ad33b709d2771604bdf
janushan12 <janu.1999.12@gmail.com>
2025-08-25 21:41:56 +0530

    PORT value also included in the .env file

--- backend/index.js
+++ backend/index.js
@@ -36,6 +36,8 @@ app.post("/send", async (req, res) => {
     }
 });
 
-app.listen(process.env.PORT || 5000, ()=> {
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, ()=> {
     console.log("Server running on port 3000");
 })
