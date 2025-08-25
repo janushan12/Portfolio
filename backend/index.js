4d48462233f852394554ee463a7cddb7dec37930
janushan12 <janu.1999.12@gmail.com>
2025-08-25 21:50:53 +0530

    mistake in index.js

--- backend/index.js
+++ backend/index.js
@@ -39,5 +39,5 @@ app.post("/send", async (req, res) => {
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, ()=> {
-    console.log("Server running on port 3000");
+    console.log(`Server running on port ${PORT}`);
 })
