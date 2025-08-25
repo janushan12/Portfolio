d16f6a63b043058079bc820fdd013efa966d36f8
janushan12 <janu.1999.12@gmail.com>
2025-08-26 00:07:25 +0530

    again error check

--- backend/index.js
+++ backend/index.js
@@ -33,16 +33,12 @@ app.post("/send", async (req, res) => {
             Subject: ${subject}
             Message: ${message}`,
     };
-    await transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error(error);
-        return res.status(500).json({ status: "error", error });
-      }
-    });
+    await transporter.sendMail(mailOptions);
     res
       .status(200)
       .json({ success: true, message: "Email sent successfully!" });
   } catch (error) {
+    console.error("Error sending email:", error);
     res.status(500).json({ success: false, message: "Something went wrong!" });
   }
 });
